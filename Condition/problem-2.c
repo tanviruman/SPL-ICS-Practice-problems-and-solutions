49a6d297ec2312261ac5b037d64e3819b50eaa41
Tanvir  Ahmed <ruman777ahmed@gmail.com>
2024-02-21 17:19:24 +0600

    Update problem-2.c

--- Condition/problem-2.c
+++ Condition/problem-2.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+   int number ;
+   printf("Enter a number:");
+   scanf("%d",&number);
+   if(number%2==0)
+    printf("Even");
+   else
+    printf("Odd");
+   return 0;
+}
